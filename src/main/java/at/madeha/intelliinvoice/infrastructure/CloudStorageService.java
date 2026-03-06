ac074067d8dac7cccd241b89f630012461ee0e05
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-06 20:20:27 +0100

    feat(Infra):Update CloudStorageService.java

--- src/main/java/at/madeha/intelliinvoice/infrastructure/CloudStorageService.java
+++ src/main/java/at/madeha/intelliinvoice/infrastructure/CloudStorageService.java
@@ -1,6 +1,9 @@
 
 package at.madeha.intelliinvoice.infrastructure;
-
+/*
+this class receives the file from the upload controller , which the user will upload it , the Class will
+save the file in the could storage and return the url of it , to be pass to the LLM and save in the database
+ */
 
 import at.madeha.intelliinvoice.service.StorageService;
 import jakarta.enterprise.context.ApplicationScoped;
