6abed40cbee74afb5be6b52e8ccf784f6ab57a40
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-11 06:58:28 +0100

    feat(Service/refactor): Add Helper class , to handel the S3 upload

---
+++ src/main/java/at/madeha/intelliinvoice/service/helper/InvoiceUploadService.java
@@ -0,0 +1,27 @@
+package at.madeha.intelliinvoice.service.helper;
+
+import at.madeha.intelliinvoice.service.StorageService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.io.InputStream;
+
+/*
+ * the uplaod controller recives the file from the user , call the service layer , the service layer call the storage layer
+ * to save the file and return the url, the url is n ow in the service layer to save it in the tadabase , and sent it ot the
+ * LLM to recive a json from the LLm , and save the json file in the database
+ */
+@ApplicationScoped
+public class InvoiceUploadService {
+
+    @Inject
+    StorageService storageService;
+
+    public String processUploadedInvoice(InputStream fileInput, String fileName) {
+
+        // upload image to S3
+        String imageUrl = storageService.uploadFile(fileInput, fileName);
+
+        return imageUrl;
+    }
+}
