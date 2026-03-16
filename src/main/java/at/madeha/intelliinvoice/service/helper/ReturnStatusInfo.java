fd38fd1a9803e3f2e76c39b8595ca691da80294b
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-16 16:37:47 +0100

    feat: add ReturnStatusInfo record and update ReturnService logic
    Close #68

--- src/main/java/at/madeha/intelliinvoice/service/helper/ReturnStatusInfo.java
+++ src/main/java/at/madeha/intelliinvoice/service/helper/ReturnStatusInfo.java
@@ -3,7 +3,8 @@ package at.madeha.intelliinvoice.service.helper;
 import at.madeha.intelliinvoice.business.InvoiceStatus;
 
 /*
-a helper Class to set the invoice status and the day left to return the invice
+a helper Class as Record only to hall the data  to set the invoice status and the day left to return the invoice ,  it is  the Class record only to handle data
+no methods or calculations , no need to write getter nas setter and Constructor  or to string etc... only data
  */
 public record ReturnStatusInfo(InvoiceStatus status, long daysLeft) {
 }
