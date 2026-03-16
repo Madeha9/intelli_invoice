3b37a349ef936aa7e87012180e970b54316c1137
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-16 13:37:44 +0100

    feat(Business): Update the InvoiceStatus Close #61

--- src/main/java/at/madeha/intelliinvoice/business/InvoiceStatus.java
+++ src/main/java/at/madeha/intelliinvoice/business/InvoiceStatus.java
@@ -5,5 +5,6 @@ enum are constant file that we use to save constant options to avoid the user en
 nused in the businss Invoice as database entity
  */
 public enum InvoiceStatus {
-    NEW, PAID, CANCELLED, RETURNED
+    Returnable, Invalid, CANCELLED, RETURNED,
+
 }
